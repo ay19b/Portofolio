8d1fa2adb46da63cdce6be67c8e188125d59f191
ay19b <benmerabetayoub95@gmail.com>
2023-01-03 19:53:54 +0100

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 let header = document.querySelector("header");
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
+let aboutContainer = document.querySelector(".about-container");
+let bars = document.querySelectorAll(".bars-box");
+let itemBars = document.querySelectorAll(".bars");
+let servicesBox = document.querySelectorAll(".services-box");
+let service = document.querySelector(".services-content");
+let heading = document.querySelectorAll(".heading");
 
-window.addEventListener("scroll", () => {
-  header.classList.toggle("shadow", window.scrollY > 0);
-});
 
 menu.onclick = () => {
   navbar.classList.toggle("active");
 };
-window.onscroll = () => {
-  navbar.classList.remove("active");
-};
 
+
+function scroll(){
+  if(aboutContainer.clientHeight-100<window.pageYOffset){
+    aboutContainer.classList.add("active")
+  };
+  bars.forEach(bar=>{
+    if(itemBars[0].clientHeight<window.pageYOffset){
+      bar.classList.add("show")
+    }
+  });
+
+  servicesBox.forEach(box=>{
+    if(service.clientHeight+800<window.pageYOffset){
+      box.classList.add("show")
+    }
+  });
+
+  for(let i = 0;i<=heading.length;i++){
+    if(window.pageYOffset>heading[i].offsetTop-230){
+      heading[i].classList.add("show")
+    }
+  }
+
+  header.classList.toggle("shadow", window.scrollY > 0);
+  navbar.classList.remove("active");
+}
+window.addEventListener("scroll",scroll);
